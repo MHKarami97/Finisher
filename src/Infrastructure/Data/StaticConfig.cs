ebff27ff019805acd60892c6dbdb0cdc068293f3
mhkarami97 <mhkarami97@gmail.com>
2025-12-07 08:56:43 +0330

    refactor: remove unused API names and schema constants for improved clarity

--- src/Infrastructure/Data/StaticConfig.cs
+++ src/Infrastructure/Data/StaticConfig.cs
@@ -6,18 +6,6 @@ public static class StaticConfig
     public const string IndexPrefix = "ix";
     public const string Separator = "_";
     public const string TableNameSeparator = ".";
-    public const string DesignRequestSchema = $"design{TableNameSeparator}request";
-    public const string ExpertOpinionSchema = $"expert{TableNameSeparator}opinion";
     public const string ConfigSchema = "dbo";
-    public const string BannerSchema = "banner";
-    public const string BlogSchema = "blog";
-    public const string CartSchema = "cart";
-    public const string ClubSchema = "club";
-    public const string FindSchema = "find";
-    public const string MainSchema = "main";
-    public const string PostSchema = "post";
-    public const string StoreSchema = "store";
-    public const string SupportSchema = "support";
     public const string UserSchema = "user";
-    public const string WarehouseSchema = "warehouse";
 }
--- src/Web/Common/ApiName.cs
+++ src/Web/Common/ApiName.cs
@@ -2,71 +2,8 @@ namespace Finisher.Web.Common;
 
 internal static class ApiName
 {
-    public static readonly string Ad = nameof(Ad).ToLowerInvariant();
-    public static readonly string Banner = nameof(Banner).ToLowerInvariant();
-    public static readonly string Blog = nameof(Blog).ToLowerInvariant();
-    public static readonly string BlogTag = nameof(BlogTag).ToLowerInvariant();
-    public static readonly string Cart = nameof(Cart).ToLowerInvariant();
-    public static readonly string CartItem = nameof(CartItem).ToLowerInvariant();
-    public static readonly string CartReturn = nameof(CartReturn).ToLowerInvariant();
-    public static readonly string Discount = nameof(Discount).ToLowerInvariant();
-    public static readonly string DiscountCategory = nameof(DiscountCategory).ToLowerInvariant();
-    public static readonly string DiscountStore = nameof(DiscountStore).ToLowerInvariant();
-    public static readonly string DiscountUser = nameof(DiscountUser).ToLowerInvariant();
-    public static readonly string SystemCart = nameof(SystemCart).ToLowerInvariant();
-    public static readonly string Club = nameof(Club).ToLowerInvariant();
-    public static readonly string ClubItem = nameof(ClubItem).ToLowerInvariant();
-    public static readonly string UserClubItem = nameof(UserClubItem).ToLowerInvariant();
-    public static readonly string DesignCart = nameof(DesignCart).ToLowerInvariant();
-    public static readonly string DesignRequest = nameof(DesignRequest).ToLowerInvariant();
-    public static readonly string DesignRequestFeature = nameof(DesignRequestFeature).ToLowerInvariant();
-    public static readonly string DesignRequestSupport = nameof(DesignRequestSupport).ToLowerInvariant();
-    public static readonly string ExpertOpinion = nameof(ExpertOpinion).ToLowerInvariant();
-    public static readonly string ExpertOpinionAnswer = nameof(ExpertOpinionAnswer).ToLowerInvariant();
-    public static readonly string FindItem = nameof(FindItem).ToLowerInvariant();
-    public static readonly string FindItemAnswer = nameof(FindItemAnswer).ToLowerInvariant();
-    public static readonly string Category = nameof(Category).ToLowerInvariant();
-    public static readonly string City = nameof(City).ToLowerInvariant();
-    public static readonly string Faq = nameof(Faq).ToLowerInvariant();
-    public static readonly string FileData = nameof(FileData).ToLowerInvariant();
-    public static readonly string Key = nameof(Key).ToLowerInvariant();
-    public static readonly string Report = nameof(Report).ToLowerInvariant();
-    public static readonly string Tag = nameof(Tag).ToLowerInvariant();
-    public static readonly string Value = nameof(Value).ToLowerInvariant();
-    public static readonly string Comment = nameof(Comment).ToLowerInvariant();
-    public static readonly string CommentFeature = nameof(CommentFeature).ToLowerInvariant();
-    public static readonly string CommentRate = nameof(CommentRate).ToLowerInvariant();
-    public static readonly string Post = nameof(Post).ToLowerInvariant();
-    public static readonly string PostFeature = nameof(PostFeature).ToLowerInvariant();
-    public static readonly string PostItemFeature = nameof(PostItemFeature).ToLowerInvariant();
-    public static readonly string PostImage = nameof(PostImage).ToLowerInvariant();
-    public static readonly string PostItem = nameof(PostItem).ToLowerInvariant();
-    public static readonly string PostQuestion = nameof(PostQuestion).ToLowerInvariant();
-    public static readonly string PostRate = nameof(PostRate).ToLowerInvariant();
-    public static readonly string PostTag = nameof(PostTag).ToLowerInvariant();
-    public static readonly string PostVideo = nameof(PostVideo).ToLowerInvariant();
-    public static readonly string PostView = nameof(PostView).ToLowerInvariant();
-    public static readonly string SpecificPost = nameof(SpecificPost).ToLowerInvariant();
-    public static readonly string Store = nameof(Store).ToLowerInvariant();
-    public static readonly string StoreCheckout = nameof(StoreCheckout).ToLowerInvariant();
-    public static readonly string StoreCredit = nameof(StoreCredit).ToLowerInvariant();
-    public static readonly string StoreFeature = nameof(StoreFeature).ToLowerInvariant();
-    public static readonly string Support = nameof(Support).ToLowerInvariant();
-    public static readonly string SupportAnswer = nameof(SupportAnswer).ToLowerInvariant();
-    public static readonly string UserAddress = nameof(UserAddress).ToLowerInvariant();
-    public static readonly string UserFavoriteList = nameof(UserFavoriteList).ToLowerInvariant();
-    public static readonly string UserFollow = nameof(UserFollow).ToLowerInvariant();
-    public static readonly string UserNotice = nameof(UserNotice).ToLowerInvariant();
     public static readonly string Identity = nameof(Identity).ToLowerInvariant();
-    public static readonly string Story = nameof(Story).ToLowerInvariant();
     public static readonly string Logger = nameof(Logger).ToLowerInvariant();
-    public static readonly string CartPay = nameof(CartPay).ToLowerInvariant();
-    public static readonly string CartShipment = nameof(CartShipment).ToLowerInvariant();
-    public static readonly string PostItemChange = nameof(PostItemChange).ToLowerInvariant();
-    public static readonly string UserList = nameof(UserList).ToLowerInvariant();
-    public static readonly string UserListItem = nameof(UserListItem).ToLowerInvariant();
-    public static readonly string Inventory = nameof(Inventory).ToLowerInvariant();
-    public static readonly string Warehouse = nameof(Warehouse).ToLowerInvariant();
 }
 
 internal static class ApiPostFixName
@@ -74,5 +11,4 @@ internal static class ApiPostFixName
     public static readonly string Popular = nameof(Popular).ToLowerInvariant();
     public static readonly string MostSell = nameof(MostSell).ToLowerInvariant();
     public static readonly string MostView = nameof(MostView).ToLowerInvariant();
-    public static readonly string WholeSale = nameof(WholeSale).ToLowerInvariant();
 }
